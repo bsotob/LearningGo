1bfd9d3ca5403441303a4baeb1a095a0ea4cef14
Unknown <brain.agam@gmail.com>
2017-10-09 08:19:21 +0200

    binary search in go

---
+++ binarySearch/binarySearch.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func binarySearch(v []int, x, left, right int) int {
+	if left <= right {
+		middle := (left + right) / 2
+		if x < v[middle] {
+			return binarySearch(v, x, left, middle-1)
+		} else if x > v[middle] {
+			return binarySearch(v, x, middle+1, right)
+		} else {
+			return middle
+		}
+	}
+	return -1
+}
+
+func main() {
+	v := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	x := -2
+	if binarySearch(v, x, 0, len(v)-1) != -1 {
+		fmt.Println("EXIST")
+	}
+}
