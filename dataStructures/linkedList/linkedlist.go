53580581a14b528e14a4eda2792f1e8104553cce
Unknown <brain.agam@gmail.com>
2017-10-24 07:54:06 +0200

    nevermind awful exemple

---
+++ dataStructures/linkedList/linkedlist.go
@@ -0,0 +1,18 @@
+//awful exemple should recode this with an real exemple
+package main
+
+import "fmt"
+
+//Node is a node
+type Node struct {
+	value string
+	next  *Node
+}
+
+//List of students where the firts in the line be always be the teacher
+func main() {
+	student := &Node{"student1", nil}
+	teacher := &Node{"Teacher", student}
+	fmt.Println(teacher)
+	fmt.Println(student)
+}
