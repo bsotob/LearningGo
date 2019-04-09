d97a077394e3239c1670dcbf1bc8b9d54bc091b2
Abner Andino <aandino@externos.itnow.es>
2019-04-09 12:06:47 +0200

    hashtable in golang is interpreted as a map

--- crackinInterview/dataStructure/hashTable.go
+++ crackinInterview/dataStructure/hashTable.go
@@ -1,4 +1,14 @@
 package main
 
+import "log"
+
 func main() {
+	// m its a map internally golang implements it as a hash table.
+	m := make(map[string]int)
+	m["test"] = 0
+	if val, ok := m["test"]; ok {
+		log.Printf("The key searched is %s and has a value of %d\n", "test", val)
+	} else {
+		log.Fatalln("The key doesnt exists")
+	}
 }
